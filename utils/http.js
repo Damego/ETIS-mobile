17810551a8773651eb7c12644d8c941faf10f617
Damego <danyabatueff@gmail.com>
2022-10-25 18:02:39 +0500

    refactor: Добавлена проверка на наличие куки

--- utils/http.js
+++ utils/http.js
@@ -24,7 +24,12 @@ export default class HTTPClient {
       headers: { "Content-Type": "multipart/form-data" },
     });
 
-    var cookies = response.headers["set-cookie"];
+    let cookies = response.headers["set-cookie"];
+    if (!cookies) {
+      console.warn(
+        "No cookies! Make sure you passed right login and password. Also maybe you have reached auth limit (5). Try again in 10 minutes."
+      );
+    }
     if (cookies != undefined) {
       // TODO: return exception message?
       this.sessionID = cookies[0].split(";")[0];
