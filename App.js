69101b97e285060141060ad38dc82f91141742b4
OKeylo <zabaev228@gmail.com>
2022-10-25 17:42:57 +0500

    init parser

--- App.js
+++ App.js
@@ -5,17 +5,20 @@ import { StyleSheet, SafeAreaView } from 'react-native';
 import Header from './components/Header';
 import Form from './components/Form';
 import HTTPClient from './utils/http';
+import DataParsing from './utils/parser';
 
 class App extends Component {
   constructor() {
     super();
 
     this.httpClient = new HTTPClient();
+    this.parser = new DataParsing();
   }
 
   defaultAuth = async (login, password, token) => {
     await this.httpClient.login(login, password, token);
-    await this.httpClient.getTimeTable();
+    let html = await this.httpClient.getTimeTable();
+    let text = this.parser.getDataTimeTable(html);
   };
 
   render() {
---
+++ utils/parser.js
@@ -0,0 +1,11 @@
+const cheerio = require('cheerio');
+
+export default class DataParsing {
+
+    getDataTimeTable(html) {
+        console.log(html);
+        const $ = cheerio.load(html);
+        let header = $('body > div.navbar.navbar-static-top').text();
+        return header;
+    }
+}
