ff24cc82de33a9d858573a4fc9945b6defb79634
Damego <Damego@users.noreply.github.com>
2022-10-26 15:53:16 +0000

    Prettified Code!

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 
-
-export default function Header({text}) {
-
+export default function Header({ text }) {
   return (
     <SafeAreaView style={styles.main}>
       <Text style={styles.text}>{text}</Text>
