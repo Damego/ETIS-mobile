e7e71f72fd1dd483ec679875041d1939e08de1d3
Damego <Damego@users.noreply.github.com>
2022-10-26 15:51:18 +0000

    Prettified Code!

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, SafeAreaView, Text } from "react-native";
 
-export default function Header({text}) {
+export default function Header({ text }) {
   return (
     <SafeAreaView style={styles.main}>
-        <Text style={styles.text}>{text}</Text>
+      <Text style={styles.text}>{text}</Text>
     </SafeAreaView>
   );
 }
 
-
 const styles = StyleSheet.create({
-    main: {
-        paddingTop: 45,
-        height: 90,
-        backgroundColor: '#C62E3E'
-    },
-    text: {
-        fontSize: 18,
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    }
+  main: {
+    paddingTop: 45,
+    height: 90,
+    backgroundColor: "#C62E3E",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
