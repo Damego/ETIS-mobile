abcf3d5d0b7b4b36dccd6115eb34de8caec67b6a
Damego <danyabatueff@gmail.com>
2022-10-26 20:51:46 +0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
 
+
 export default function Header({text}) {
   return (
     <SafeAreaView style={styles.main}>
