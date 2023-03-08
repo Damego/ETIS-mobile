26517f4e6d6a5b598920b5bbc717e6028ff5de20
Damego <danyabatueff@gmail.com>
2023-03-08 21:55:44 +0500

    refactor: re style teach plan subject

--- screens/shortTeachPlan/Subject.jsx
+++ screens/shortTeachPlan/Subject.jsx
@@ -3,46 +3,48 @@ import { View, Text, StyleSheet } from "react-native";
 
 const Subject = ({ data }) => {
   return (
-    <View>
-        <View style={styles.subjectNameView}>
-            <Text style={styles.subjectNameText}>{data.subject}</Text>
-        </View>
+    <View style={styles.subjectView}>
+      <View style={styles.subjectNameView}>
+        <Text style={styles.subjectNameText}>{data.subject}</Text>
+      </View>
 
-        <View style={styles.subjectInfoView}>
-            <Text style={styles.subjectInfoText}>{data.reporting}</Text>
-            <Text style={styles.subjectInfoText}>{`Аудит.часы: ${data.classWork}`}</Text>
-            <Text style={styles.subjectInfoText}>{`Сам.часы: ${data.soloWork}`}</Text>
-            <Text style={styles.subjectInfoText}>{`Всего: ${data.total}`}</Text>
-        </View>
+      <View style={styles.subjectInfoView}>
+        <Text style={styles.subjectInfoText}>{data.reporting}</Text>
+        <Text
+          style={styles.subjectInfoText}
+        >{`Аудит.часы: ${data.classWork}`}</Text>
+        <Text
+          style={styles.subjectInfoText}
+        >{`Сам.часы: ${data.soloWork}`}</Text>
+        <Text style={styles.subjectInfoText}>{`Всего: ${data.total}`}</Text>
+      </View>
     </View>
   );
 };
 
 export default Subject;
 
-
 const styles = StyleSheet.create({
-    subjectNameView: {
-        marginLeft: "1%",
-        display: "flex",
-        marginTop: "1%",
-        marginBottom: "1%",
-        flexShrink: 1,
+  subjectView: {
+    paddingBottom: "1%",
+  },
+  subjectNameView: {
+    marginLeft: "1%",
+    marginTop: "1%",
+    marginBottom: "1%",
 
-        paddingHorizontal: "1%",
-    },
-    subjectNameText: {
-        fontSize: 16,
-        fontWeight: "400"
-    },
-    subjectInfoView: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-around"
-    },
-    subjectInfoText: {
-        fontSize: 13,
-        color: "#323232",
-
-    }
-})
+    paddingHorizontal: "1%",
+  },
+  subjectNameText: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  subjectInfoView: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  subjectInfoText: {
+    fontSize: 13,
+    color: "#1c1c1c",
+  },
+});
