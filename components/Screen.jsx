a56b83ff77ab2a15d3eae5d29c486f510d885950
Damego <danyabatueff@gmail.com>
2023-03-16 21:32:53 +0500

    fix: incorrect statusbar size

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView, View, StyleSheet } from 'react-native';
-
+import Constants from 'expo-constants';
 import Header from './Header';
 
 const styles = StyleSheet.create({
@@ -11,12 +11,14 @@ const styles = StyleSheet.create({
 });
 
 const Screen = ({ headerText, children }) => (
-  <ScrollView showsVerticalScrollIndicator={false} overScrollMode='never'>
-    <View style={styles.screen}>
-      <Header text={headerText} />
-      {children}
-    </View>
-  </ScrollView>
+  <View style={{marginTop: Constants.statusBarHeight }}>
+    <ScrollView showsVerticalScrollIndicator={false} overScrollMode='never'>
+      <View style={styles.screen}>
+        <Header text={headerText} />
+        {children}
+      </View>
+    </ScrollView>
+  </View>
 );
 
 export default Screen;
