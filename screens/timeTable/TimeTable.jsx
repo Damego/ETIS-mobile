9b2ecf6631790533bb0e9c1ee7119680b41422d6
Damego <danyabatueff@gmail.com>
2023-04-26 22:29:26 +0500

    refactor: add refreshing to timetable & clean up code. Part 1

--- screens/timeTable/TimeTable.jsx
+++ screens/timeTable/TimeTable.jsx
@@ -1,78 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import 'react-native-get-random-values';
-import { v4 as uuid4 } from 'uuid';
 
 import LoadingPage from '../../components/LoadingPage';
+import Screen from '../../components/Screen';
+import { GLOBAL_STYLES } from '../../styles/styles';
+import { vars } from '../../utils/vars';
 import { Day, EmptyDay } from './Day';
 import WeekNavigation from './WeekNavigator';
-import Screen from '../../components/Screen';
-
-import { vars } from '../../utils/vars';
-import { GLOBAL_STYLES } from '../../styles/styles';
 
 const TimeTablePage = () => {
-  const [isLoaded, setLoaded] = useState(false);
   const [data, setData] = useState(null);
+  const [week, changeWeek] = useState(null);
 
-  /**
-   *
-   * @param {number | null} week День недели
-   */
-  const getWeekData = async (week = null) => {
-    // TODO: cache data
-
+  const loadData = async () => {
     let html;
+    console.log('current week', week);
+    // TODO:
+    //  When I refresh page it uses previous state
+
     if (week != null) {
       html = await vars.httpClient.getTimeTable({ week });
     } else {
       html = await vars.httpClient.getTimeTable();
     }
-    // TODO: What the fuck why navigation doesn't work?
-    // if (vars.parser.isLoginPage(html)) {
-    //   navigation.navigate("StackNavigator", {screen: "Authorization"});
-    //   return;
-    // }
 
-    if (!html) return null;
-    return vars.parser.parseTimeTable(html);
+    if (!html) {
+      console.warn('failed to load timetable');
+      return;
+    }
+    const parsedData = vars.parser.parseTimeTable(html);
+    setData(parsedData);
   };
 
   useEffect(() => {
-    if (isLoaded) return;
+    console.log('useeffect week', week);
+    loadData();
+  }, [week, setData]);
 
-    const wrapper = async () => {
-      const res = await getWeekData();
-      if (res != null) {
-        setLoaded(true);
-        setData(res);
-      }
-    };
-    wrapper();
-  }, [isLoaded, setLoaded, setData]);
-
-  const changeWeek = async (week) => {
-    const res = await getWeekData(week);
-    setData(res);
-  };
-
-  if (!isLoaded || !data) return <LoadingPage />;
+  if (!data) return <LoadingPage />;
 
   return (
-    <Screen headerText="Расписание">
+    <Screen headerText="Расписание" onUpdate={loadData}>
       <WeekNavigation
         firstWeek={data.firstWeek}
         lastWeek={data.lastWeek}
         currentWeek={data.currentWeek}
-        onWeekChange={(week) => changeWeek(week)}
+        onWeekChange={(selectedWeek) => changeWeek(selectedWeek)}
       />
 
       <View style={GLOBAL_STYLES.daysView}>
         {data.days.map((day) =>
           day.lessons.length === 0 ? (
-            <EmptyDay key={uuid4()} data={day} />
+            <EmptyDay key={day.date} data={day} />
           ) : (
-            <Day key={uuid4()} data={day} />
+            <Day key={day.date} data={day} />
           )
         )}
       </View>
