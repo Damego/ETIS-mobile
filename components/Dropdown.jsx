1da2193bf901172e24f59e857a1df09047fc583c
Damego <danyabatueff@gmail.com>
2023-05-06 19:43:40 +0500

    fix&ref(dropdown): remove extra pressable comp

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,18 +1,18 @@
+import { AntDesign } from '@expo/vector-icons';
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
 
 import { GLOBAL_STYLES } from '../styles/styles';
 
 const styles = StyleSheet.create({
   dropdownView: {
-    position: "relative"
+    position: 'relative',
   },
   selectButton: {
     paddingVertical: '1%',
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
-    display: "flex",
+    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: '3%',
@@ -24,46 +24,43 @@ const styles = StyleSheet.create({
   },
   menuView: {
     position: 'absolute',
-    backgroundColor: "#FFFFFF",
-    top: "100%",
-    width: "100%",
+    backgroundColor: '#FFFFFF',
+    top: '100%',
+    width: '100%',
     borderRadius: 10,
   },
   optionView: {
     paddingHorizontal: '3%',
-    paddingVertical: "2%"
+    paddingVertical: '2%',
   },
   optionText: {
     fontSize: 16,
     fontWeight: '500',
-  }
+  },
 });
 
-function SelectOption({label, value, onClick}) {
+function SelectOption({ label }) {
   return (
-    <Pressable onPress={() => onClick(value)}>
-      <View style={styles.optionView}>
-        <Text style={styles.optionText}>{label}</Text>
-      </View>
-    </Pressable>
-  )
-}
-
-function Menu({options, onSelect}) {
-  return (
-    <View style={[styles.menuView, GLOBAL_STYLES.shadow]}>
-      {
-        options.map(({ label, value }) => (
-          <Pressable onPress={() => onSelect(value)} key={`pressable-${label}`}>
-            <SelectOption label={label} value={value} key={label}/>
-          </Pressable>
-        ))
-      }
+    <View style={styles.optionView}>
+      <Text style={styles.optionText}>{label}</Text>
     </View>
   );
 }
 
-function Select({selectedOption, isOpened, toggleOpened}) {
+function Menu({ options, onSelect }) {
+
+  return (
+    <View style={[styles.menuView, GLOBAL_STYLES.shadow]}>
+      {options.map(({ label, value }) => (
+        <Pressable onPress={() => onSelect(value)} key={`pressable-${label}`}>
+          <SelectOption label={label} key={label} />
+        </Pressable>
+      ))}
+    </View>
+  );
+}
+
+function Select({ selectedOption, isOpened, toggleOpened }) {
   return (
     <Pressable onPress={toggleOpened}>
       <View style={[styles.selectButton, GLOBAL_STYLES.shadow]}>
@@ -71,10 +68,10 @@ function Select({selectedOption, isOpened, toggleOpened}) {
         <AntDesign name={isOpened ? 'caretup' : 'caretdown'} size={14} color="black" />
       </View>
     </Pressable>
-  )
+  );
 }
 
-export default function Dropdown({selectedOption, options, onSelect}) {
+export default function Dropdown({ selectedOption, options, onSelect }) {
   const [isOpened, setOpened] = useState(false);
 
   const toggleOpened = () => {
@@ -83,8 +80,12 @@ export default function Dropdown({selectedOption, options, onSelect}) {
 
   return (
     <View style={styles.dropdownView}>
-      <Select selectedOption={selectedOption.label} isOpened={isOpened} toggleOpened={toggleOpened}/>
-      {isOpened ? <Menu options={options} onSelect={onSelect}/> : ''}
+      <Select
+        selectedOption={selectedOption.label}
+        isOpened={isOpened}
+        toggleOpened={toggleOpened}
+      />
+      {isOpened ? <Menu options={options} onSelect={onSelect} /> : ''}
     </View>
   );
 }
