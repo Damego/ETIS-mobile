05650b08ed396dd25b6b3bf15bb5b6457e1d25a8
Damego <danyabatueff@gmail.com>
2023-05-06 20:41:44 +0500

    feat: implement trimester dropdown

--- screens/signs/Signs.jsx
+++ screens/signs/Signs.jsx
@@ -7,26 +7,56 @@ import Screen from '../../components/Screen';
 import { vars } from '../../utils/vars';
 import CardSign from './CardSign';
 
+function buildTrimesterOptions(currentTrimester, latestTrimester) {
+  const options = [];
+  let index = 0;
+
+  while (index <= latestTrimester) {
+    index += 1
+    if (index === currentTrimester) continue;
+
+    options.push({
+      label: `${index} Триместр`,
+      value: index,
+    });
+  }
+
+  return {
+    current: {
+      label: `${currentTrimester} Триместр`,
+      value: currentTrimester,
+    },
+    options,
+  };
+}
+
 const Signs = () => {
   const [data, setData] = useState(null);
+  const [optionData, setOptionData] = useState(null);
 
-  const loadData = async () => {
-    const html = await vars.httpClient.getSigns('current');
+  const loadData = async (trimester) => {
+    const html = await vars.httpClient.getSigns('current', { trimester });
     if (vars.parser.isLoginPage(html)) return; // TODO: move to auth page
 
     const parsedData = vars.parser.parseSigns(html);
+    setOptionData(buildTrimesterOptions(parsedData.currentTrimester, parsedData.latestTrimester));
     setData(parsedData);
   };
 
   useEffect(() => {
-    if (data) return;
     loadData();
-  }, [data]);
+  }, []);
 
-  if (!data) return <LoadingPage />;
-
+  if (!data || !optionData) return <LoadingPage />;
   return (
     <Screen headerText="Оценки" onUpdate={loadData}>
+      <View style={{ marginLeft: 'auto', marginRight: 0, paddingBottom: '2%', zIndex: 1 }}>
+        <Dropdown
+          onSelect={loadData}
+          selectedOption={optionData.current}
+          options={optionData.options}
+        />
+      </View>
       {data.subjects.map((subject) => (
         <CardSign subject={subject} key={subject.subject} />
       ))}
