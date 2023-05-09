d8ea14e445588e447fad4b9455c3c2333a1cbec4
Damego <danyabatueff@gmail.com>
2023-05-09 11:49:18 +0500

    refactor: change variable names in calculate limits

--- utils/funcs.js
+++ utils/funcs.js
@@ -1,15 +1,19 @@
-const calculateLimits = (firstWeek, currentWeek, lastWeek) => {
+const calculateLimits = (firstPage, currentPage, lastPage) => {
   const limits = 3;
-  let leftLimit = currentWeek - limits;
-  let rightLimit = currentWeek + limits;
-
-  if (currentWeek - firstWeek <= limits) {
-    leftLimit = firstWeek;
-    rightLimit = firstWeek + limits * 2;
+  if (lastPage - firstPage <= limits * 2) {
+    return { leftLimit: firstPage, rightLimit: lastPage };
   }
-  if (lastWeek - currentWeek <= limits) {
-    rightLimit = lastWeek;
-    leftLimit = lastWeek - limits * 2;
+
+  let leftLimit = currentPage - limits;
+  let rightLimit = currentPage + limits;
+
+  if (currentPage - firstPage <= limits) {
+    leftLimit = firstPage;
+    rightLimit = firstPage + limits * 2;
+  }
+  if (lastPage - currentPage <= limits) {
+    rightLimit = lastPage;
+    leftLimit = lastPage - limits * 2;
   }
 
   return { leftLimit, rightLimit };
