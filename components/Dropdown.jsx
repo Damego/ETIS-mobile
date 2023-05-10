b6bed1b559e0bc7b466bd89160f722c337f652fd
Damego <danyabatueff@gmail.com>
2023-05-10 13:42:53 +0500

    styles: add opacity to clickable component

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -52,7 +52,7 @@ function Menu({ options, onSelect }) {
   return (
     <View style={[styles.menuView, GLOBAL_STYLES.shadow]}>
       {options.map(({ label, value }) => (
-        <TouchableOpacity onPress={() => onSelect(value)} key={`pressable-${label}`}>
+        <TouchableOpacity onPress={() => onSelect(value)} key={`pressable-${label}`} activeOpacity={0.9}>
           <SelectOption label={label} key={label} />
         </TouchableOpacity>
       ))}
@@ -62,7 +62,7 @@ function Menu({ options, onSelect }) {
 
 function Select({ selectedOption, isOpened, toggleOpened }) {
   return (
-    <TouchableOpacity onPress={toggleOpened}>
+    <TouchableOpacity onPress={toggleOpened} activeOpacity={0.9}>
       <View style={[styles.selectButton, GLOBAL_STYLES.shadow]}>
         <Text style={styles.selectText}>{selectedOption}</Text>
         <AntDesign name={isOpened ? 'caretup' : 'caretdown'} size={14} color="black" />
