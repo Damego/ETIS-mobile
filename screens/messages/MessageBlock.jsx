d7808e63a4537126705305c7a249ccba5e150dbd
Damego <danyabatueff@gmail.com>
2023-05-13 20:01:09 +0500

    chore: rename `sortMessages` to `compareMessages`

--- screens/messages/MessageBlock.jsx
+++ screens/messages/MessageBlock.jsx
@@ -8,7 +8,7 @@ export default function MessageBlock({ route, navigation }) {
   const [name1, name2, name3] = data[0].author.split(' ');
   const author = `${name1} ${name2.charAt(0)}. ${name3.charAt(0)}.`;
 
-  const sortMessages = (first, second) => {
+  const compareMessages = (first, second) => {
     if (first.time < second.time) return -1
     if (first.time > second.time) return 1;
     return 0
@@ -16,7 +16,7 @@ export default function MessageBlock({ route, navigation }) {
 
   return (
     <Screen headerText={author} onBackPageClick={() => navigation.navigate('Все сообщения')}>
-      {data.sort(sortMessages).map((message) => (
+      {data.sort(compareMessages).map((message) => (
         <Message data={message} key={message.time.format()} />
       ))}
     </Screen>
