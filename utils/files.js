0f72379d929c8b97b6d64227337ce08626e19acb
Damego <Damego@users.noreply.github.com>
2023-05-14 12:22:11 +0000

    ci: prettified code

--- utils/files.js
+++ utils/files.js
@@ -5,8 +5,8 @@ import {
   writeAsStringAsync,
 } from 'expo-file-system';
 import { shareAsync } from 'expo-sharing';
-import {getDocumentAsync} from 'expo-document-picker';
-import {PermissionsAndroid} from 'react-native';
+import { getDocumentAsync } from 'expo-document-picker';
+import { PermissionsAndroid } from 'react-native';
 
 import httpClient from './http';
 
@@ -28,16 +28,17 @@ const saveFile = async (fileData, fileName) => {
 
 const checkReadStoragePermission = () => {
   return PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
-}
+};
 
 const requestReadStoragePermission = async () => {
-  const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+  );
   return granted === PermissionsAndroid.RESULTS.GRANTED;
-
-}
+};
 
 const selectFile = async () => {
-  const hasPerms = await checkReadStoragePermission()
+  const hasPerms = await checkReadStoragePermission();
   if (!hasPerms) {
     const result = await requestReadStoragePermission();
     if (!result) return;
@@ -45,7 +46,7 @@ const selectFile = async () => {
 
   const documentResult = await getDocumentAsync();
   if (documentResult.type === 'cancel') return;
-  return documentResult
-}
+  return documentResult;
+};
 
 export { downloadFile, saveFile, selectFile };
