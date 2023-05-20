da494d6651bbd970f65c8edd4dfe40195d542c57
Damego <danyabatueff@gmail.com>
2023-05-20 09:43:36 +0500

    refactor: use Stack instead of Native Stack in message stack

--- navigation/MessageStackNavigator.jsx
+++ navigation/MessageStackNavigator.jsx
@@ -1,10 +1,10 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 
 import MessageBlock from '../screens/messages/MessageBlock';
 import MessageHistory from '../screens/messages/MessageHistory';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 function MessageStackNavigator() {
   return (
