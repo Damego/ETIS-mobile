baf63ce3ec3259a066afb7d4ba5d06b0769adc82
Damego <Damego@users.noreply.github.com>
2023-05-21 05:26:15 +0000

    ci: prettified code

--- redux/authSlice.js
+++ redux/authSlice.js
@@ -1,22 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
 const authSlice = createSlice({
   name: 'authSlice',
   initialState: {
     isSignedIn: false,
-    shouldAutoAuth: false
+    shouldAutoAuth: false,
   },
   reducers: {
     signIn(state) {
-      state.isSignedIn = true
+      state.isSignedIn = true;
     },
     signOut(state, action) {
       state.isSignedIn = false;
-      state.shouldAutoAuth = action.payload.autoAuth
-    }
-  }
-})
+      state.shouldAutoAuth = action.payload.autoAuth;
+    },
+  },
+});
 
 export default authSlice.reducer;
-export const {signIn, signOut} = authSlice.actions
+export const { signIn, signOut } = authSlice.actions;
--- redux/index.js
+++ redux/index.js
@@ -10,4 +10,4 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
-export default store
+export default store;
