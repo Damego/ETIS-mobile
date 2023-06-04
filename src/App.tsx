eada0053d104905c9edece9d23c4f3ea1774ad7c
Damego <danyabatueff@gmail.com>
2023-06-04 14:34:18 +0500

    refactor: move app to src

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 
-import StackNavigator from './src/navigation/StackNavigator';
-import setupStore from './src/redux';
-import { loadSettings, loadUserCredentials } from './src/redux/storageLoader';
+import StackNavigator from './navigation/StackNavigator';
+import setupStore from './redux';
+import { loadSettings, loadUserCredentials } from './redux/storageLoader';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -16,10 +16,10 @@ store.dispatch(loadUserCredentials());
 
 const App = () => {
   const [fontsLoaded] = useFonts({
-    'Nunito-SemiBold': require('./assets/fonts/Nunito-SemiBold.ttf'),
-    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
-    'Nunito-Light': require('./assets/fonts/Nunito-Light.ttf'),
-    'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
+    'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf'),
+    'Nunito-Light': require('../assets/fonts/Nunito-Light.ttf'),
+    'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
   });
 
   useEffect(() => {
