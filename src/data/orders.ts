9db3cf86c90f20dc1630550a1e1163d41b7ea8f6
Damego <danyabatueff@gmail.com>
2023-06-14 21:47:11 +0500

    fix&ref: pass http method; typing & log stuff

--- src/data/orders.ts
+++ src/data/orders.ts
@@ -1,8 +1,8 @@
-import { emptyResult, IGetPayload, IGetResult } from '../models/results';
-import { httpClient, storage } from '../utils';
-import { isLoginPage } from '../parser/utils';
-import parseOrders from '../parser/order';
 import { IOrder } from '../models/order';
+import { IGetPayload, IGetResult, emptyResult } from '../models/results';
+import parseOrders from '../parser/order';
+import { isLoginPage } from '../parser/utils';
+import { httpClient, storage } from '../utils';
 import { HTTPError } from '../utils/http';
 
 export const getCachedOrders = async (): Promise<IGetResult<IOrder[]>> => {
@@ -11,12 +11,12 @@ export const getCachedOrders = async (): Promise<IGetResult<IOrder[]>> => {
     ...emptyResult,
     data: await storage.getOrdersData(),
   };
-}
+};
 
 export const getOrdersData = async (payload: IGetPayload): Promise<IGetResult<IOrder[]>> => {
   if (payload.useCacheFirst) {
     const result = await getCachedOrders();
-    if (result.data) return result
+    if (result.data) return result;
   }
 
   const response = await httpClient.getOrders();
@@ -26,24 +26,24 @@ export const getOrdersData = async (payload: IGetPayload): Promise<IGetResult<IO
     return emptyResult;
   }
 
-  if (isLoginPage(response)) return { ...emptyResult, isLoginPage: true };
+  if (isLoginPage(response as string)) return { ...emptyResult, isLoginPage: true };
 
-  const data = parseOrders(response);
+  const data = parseOrders(response as string);
   console.log('[DATA] Fetched orders data');
   cacheOrdersData(data);
 
   return {
     data,
     isLoginPage: false,
-    fetched: true
-  }
-}
+    fetched: true,
+  };
+};
 
 export const cacheOrdersData = (data: IOrder[]) => {
   console.log('[DATA] Caching orders data...');
 
   storage.storeOrdersData(data);
-}
+};
 
 export const getOrderHTML = async (order: IOrder) => {
   const cached = await storage.getOrderHTML(order.id);
@@ -52,13 +52,13 @@ export const getOrderHTML = async (order: IOrder) => {
     return cached;
   }
 
-  const fetched = await httpClient.request(`/${order.uri}`);
+  const fetched = await httpClient.request('GET', `/${order.uri}`, { returnResponse: false });
 
-  console.log("[DATA] fetched order html");
+  console.log('[DATA] fetched order html');
 
-  storage.storeOrderHTML(order.id, fetched);
+  storage.storeOrderHTML(order.id, fetched as string);
 
-  console.log("[DATA] cached order html");
+  console.log('[DATA] cached order html');
 
   return fetched;
-}
+};
