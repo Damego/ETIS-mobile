1cba38970f1cb9e74d39b00a5f677a4ceb824a2c
ijo42 <ijo42@users.noreply.github.com>
2023-06-21 19:36:12 +0000

    ci: prettified code

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,4 @@
-export const PRIVACY_POLICY_URL =
-  'https://damego.github.io/ETIS-mobile/privacy_policy.html'
+export const PRIVACY_POLICY_URL = 'https://damego.github.io/ETIS-mobile/privacy_policy.html';
 export const TELEGRAM_URL = 'https://t.me/etis_mobile';
 
 export const NOTIFICATION_GUIDE_URL = `${TELEGRAM_URL}/12`;
