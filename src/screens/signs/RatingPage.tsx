2921714a90a91f1569907c149b656f9025ed2122
Damego <danyabatueff@gmail.com>
2023-07-27 12:56:08 +0500

    feat: add top tab navigation

--- src/screens/signs/RatingPage.tsx
+++ src/screens/signs/RatingPage.tsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { getRatingData } from '../../data/rating';
 import { setAuthorizing } from '../../redux/reducers/authSlice';
-import { ToastAndroid } from 'react-native';
+import { Text, ToastAndroid } from 'react-native';
 import { useAppDispatch } from '../../hooks';
+import { IRating } from '../../models/rating';
+import LoadingScreen from '../../components/LoadingScreen';
+import Screen from '../../components/Screen';
 
 export default function RatingPage() {
-  const [fetchedFirstTime, setFetchedFirstTime] = useRef<boolean>(false);
+  const [data, setData] = useState<IRating>()
+  const fetchedFirstTime = useRef<boolean>(false);
   const dispatch = useAppDispatch()
 
   const loadData = async ({session, force}: {session: number, force: boolean}) => {
-    const result = await getRatingData({useCache: true, useCacheFirst: fetchedFirstTime, session});
+    const result = await getRatingData({useCache: true, useCacheFirst: !force && fetchedFirstTime.current, session});
 
     if (result.isLoginPage) {
       dispatch(setAuthorizing(true));
@@ -19,5 +23,17 @@ export default function RatingPage() {
       ToastAndroid.show('Упс... Нет данных для отображения', ToastAndroid.LONG);
       return;
     }
+
+    setData(result.data);
   }
+
+  if (!data) {
+    return <LoadingScreen />
+  }
+
+  return (
+    <Screen>
+      <Text>Тест</Text>
+    </Screen>
+  )
 }
