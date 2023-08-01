81d32a6d6e05359bdf369c5fb3c865e7bc1cd65d
Damego <damego.dev@gmail.com>
2023-08-01 11:22:17 +0500

    feat: implement rating page

--- src/screens/signs/RatingPage.tsx
+++ src/screens/signs/RatingPage.tsx
@@ -1,19 +1,49 @@
-import React, { useRef, useState } from 'react';
-import { getRatingData } from '../../data/rating';
-import { setAuthorizing } from '../../redux/reducers/authSlice';
-import { Text, ToastAndroid } from 'react-native';
-import { useAppDispatch } from '../../hooks';
-import { IRating } from '../../models/rating';
+import React, { useEffect, useRef, useState } from 'react';
+import { Text, ToastAndroid, View } from 'react-native';
+
+import BorderLine from '../../components/BorderLine';
+import CardHeaderOut from '../../components/CardHeaderOut';
 import LoadingScreen from '../../components/LoadingScreen';
 import Screen from '../../components/Screen';
+import SessionDropdown from '../../components/SessionDropdown';
+import { getRatingData } from '../../data/rating';
+import { useAppDispatch } from '../../hooks';
+import { IGroup, IRating } from '../../models/rating';
+import { setAuthorizing } from '../../redux/reducers/authSlice';
+import { fontSize } from '../../utils/texts';
+import RightText from './RightText';
+
+const Group = ({ group }: { group: IGroup }) => (
+  <CardHeaderOut topText={group.name}>
+    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <View style={{ width: '70%' }}>
+        {group.disciplines.map((discipline, index) => (
+          <View key={discipline.discipline}>
+            <Text style={fontSize.medium}>{discipline.discipline}</Text>
+            <Text style={fontSize.medium}>
+              {discipline.top} из {discipline.total}
+            </Text>
+            {index !== group.disciplines.length - 1 && <BorderLine />}
+          </View>
+        ))}
+      </View>
+
+      <RightText topText={group.overall.top} bottomText={`из ${group.overall.total}`} />
+    </View>
+  </CardHeaderOut>
+);
 
 export default function RatingPage() {
-  const [data, setData] = useState<IRating>()
+  const [data, setData] = useState<IRating>();
   const fetchedFirstTime = useRef<boolean>(false);
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-  const loadData = async ({session, force}: {session: number, force: boolean}) => {
-    const result = await getRatingData({useCache: true, useCacheFirst: !force && fetchedFirstTime.current, session});
+  const loadData = async ({ session, force }: { session?: number; force: boolean }) => {
+    const result = await getRatingData({
+      useCache: true,
+      useCacheFirst: !force && fetchedFirstTime.current,
+      session,
+    });
 
     if (result.isLoginPage) {
       dispatch(setAuthorizing(true));
@@ -25,15 +55,47 @@ export default function RatingPage() {
     }
 
     setData(result.data);
-  }
+    if (!fetchedFirstTime.current) {
+      fetchedFirstTime.current = true;
+    }
+  };
+
+  useEffect(() => {
+    loadData({ force: false });
+  }, []);
 
   if (!data) {
-    return <LoadingScreen />
+    return <LoadingScreen />;
   }
 
   return (
-    <Screen>
-      <Text>Тест</Text>
+    <Screen
+      onUpdate={() => {
+        loadData({ session: data.session.current, force: true });
+      }}
+    >
+      <View
+        style={{
+          marginTop: '2%',
+          marginLeft: 'auto',
+          marginRight: 0,
+          paddingBottom: '2%',
+          zIndex: 1,
+        }}
+      >
+        <SessionDropdown
+          currentSession={data.session.current}
+          latestSession={data.session.latest}
+          sessionName={data.session.name}
+          onSelect={(session) => {
+            loadData({ session, force: false });
+          }}
+        />
+      </View>
+
+      {data.groups.map((group) => (
+        <Group group={group} key={group.name} />
+      ))}
     </Screen>
-  )
+  );
 }
---
+++ src/screens/signs/RightText.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { useGlobalStyles } from '../../hooks';
+import { fontSize } from '../../utils/texts';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    width: '25%',
+  },
+  topText: {
+    fontWeight: '600',
+  },
+  bottomText: {
+    fontWeight: '600',
+  },
+});
+
+export default function RightText({
+  topText,
+  bottomText,
+}: {
+  topText: React.ReactNode;
+  bottomText: React.ReactNode;
+}) {
+  const globalStyles = useGlobalStyles();
+  console.log(topText, bottomText)
+
+  return (
+    <View style={styles.container}>
+      <Text style={[fontSize.xxlarge, styles.topText]}>{topText}</Text>
+      <Text style={[fontSize.medium, styles.bottomText, globalStyles.textColor]}>{bottomText}</Text>
+    </View>
+  );
+}
