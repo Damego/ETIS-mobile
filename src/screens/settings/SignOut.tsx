4dbbd08473bd3339f09319460fcbf594d899978c
Damego <damego.dev@gmail.com>
2023-08-09 19:59:14 +0500

    feat: delete user data on sign out

--- src/screens/settings/SignOut.tsx
+++ src/screens/settings/SignOut.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Alert } from 'react-native';
 
+import { cache } from '../../cache/smartCache';
 import ClickableText from '../../components/ClickableText';
 import { useAppDispatch, useGlobalStyles } from '../../hooks';
 import { signOut } from '../../redux/reducers/authSlice';
-import { storage } from '../../utils';
-import { fontSize } from '../../utils/texts';
 import { unregisterBackgroundFetchAsync } from '../../tasks/signs';
-import { cache } from '../../cache/smartCache';
+import { fontSize } from '../../utils/texts';
 
 const SignOut = () => {
   const dispatch = useAppDispatch();
   const globalStyles = useGlobalStyles();
 
   const doSignOut = async () => {
-    await cache.deleteUserCredentials();
+    await cache.clear();
     dispatch(signOut({ cleanUserCredentials: true }));
     try {
       unregisterBackgroundFetchAsync();
@@ -24,15 +23,19 @@ const SignOut = () => {
   };
 
   const confirmSignOut = () => {
-    Alert.alert('Выход из аккаунта', 'Вы действительно хотите выйти из аккаунта?', [
-      {
-        text: 'Отмена',
-      },
-      {
-        text: 'Выйти',
-        onPress: doSignOut,
-      },
-    ]);
+    Alert.alert(
+      'Выход из аккаунта',
+      'Вы действительно хотите выйти из аккаунта? Это действие удалит все ваши данные.',
+      [
+        {
+          text: 'Отмена',
+        },
+        {
+          text: 'Выйти',
+          onPress: doSignOut,
+        },
+      ]
+    );
   };
 
   return (
