9dd4d9c4b14ea606fec029437dd548925775937d
Damego <damego.dev@gmail.com>
2023-08-09 12:25:48 +0500

    feat: remove storage

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,9 @@
 import { GITHUB_URL, NOTIFICATION_GUIDE_URL, PRIVACY_POLICY_URL, TELEGRAM_URL } from './consts';
 import { downloadFile, saveFile, selectFile } from './files';
 import httpClient from './http';
-import Storage from './storage';
-
-const storage = new Storage();
 
 export {
   httpClient,
-  storage,
   downloadFile,
   saveFile,
   selectFile,
