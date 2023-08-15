94c19ddf2ff8c4e8c9d12bbbaf6b46fef08d94d1
Damego <damego.dev@gmail.com>
2023-08-15 12:07:29 +0500

    chore: style teacher step

--- src/screens/sessionTest/TeacherQuestionView.tsx
+++ src/screens/sessionTest/TeacherQuestionView.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
-import { styles } from '../auth/AuthForm';
+import { Text, TextInput, View } from 'react-native';
+
 import { useGlobalStyles } from '../../hooks';
+import { fontSize } from '../../utils/texts';
+import { styles } from '../auth/AuthForm';
 
 export default function TeacherQuestionView({
   teacher,
@@ -12,11 +14,13 @@ export default function TeacherQuestionView({
 }) {
   const globalStyles = useGlobalStyles();
   return (
-    <View>
-      <Text>{teacher}</Text>
-      <Text>Если же был другой преподаватель, то введите его</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={[fontSize.large, globalStyles.textColor]}>{teacher}</Text>
+      <Text style={[fontSize.small, globalStyles.textColor, {marginVertical: '2%'}]}>
+        Если же был другой преподаватель,{'\n'}то введите его
+      </Text>
       <TextInput
-        style={[globalStyles.textColor]}
+        style={[globalStyles.textColor, globalStyles.border, globalStyles.block, { width: '80%', paddingVertical: '2%', paddingHorizontal: '2%' }]}
         onChangeText={setTeacher}
         placeholder="Иванов И.И."
         placeholderTextColor={globalStyles.textColor.color}
