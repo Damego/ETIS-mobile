77066e7a35256f12cf577442f42d9f0be4150164
ijo42 <admin@Ijo42.ru>
2023-08-17 14:17:15 +0500

    chore: store timestamp on every key

--- src/cache/mappedCache.ts
+++ src/cache/mappedCache.ts
@@ -1,17 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-type Dict<KT extends string | number | symbol, VT> = { [key in KT]: VT };
+type Dict<K extends string | number | symbol, V> = { [key in K]: { data: V; timestamp: number } };
 
-export default class MappedCache<KT extends string | number | symbol, VT> {
+export default class MappedCache<K extends string | number | symbol, V> {
   private readonly key: string;
-  private data: Dict<KT, VT>;
-  private timestamp: number;
+  private data: Dict<K, V>;
   private ready: boolean;
 
   constructor(key: string) {
     this.key = key;
-    this.data = {} as Dict<KT, VT>;
-    this.timestamp = 0;
+    this.data = {} as Dict<K, V>;
     this.ready = false;
   }
 
@@ -20,48 +18,42 @@ export default class MappedCache<KT extends string | number | symbol, VT> {
   }
 
   async save() {
-    await AsyncStorage.setItem(
-      this.key,
-      JSON.stringify({ data: this.data, timestamp: this.timestamp })
-    );
+    await AsyncStorage.setItem(this.key, JSON.stringify(this.data));
   }
 
   async init() {
     if (this.isReady()) return;
 
     const stringData = await AsyncStorage.getItem(this.key);
-    const { data, timestamp } = JSON.parse(stringData);
-    this.data = data || {};
-    this.timestamp = timestamp;
+    this.data = JSON.parse(stringData) || {};
     this.ready = true;
 
     console.log(`[CACHE] ${this.key.toLowerCase()} is ready to work`);
   }
 
-  get(key: KT): VT {
-    return this.data[key];
+  get(key: K): V {
+    return this.data[key].data;
   }
 
-  getTime() {
-    return this.timestamp;
+  getTime(key: K): number {
+    return this.data[key].timestamp;
   }
 
-  place(key: KT, value: VT) {
-    this.data[key] = value;
-    this.timestamp = Date.now();
+  place(key: K, value: V) {
+    this.data[key] = { data: value, timestamp: Date.now() };
   }
 
-  delete(key: KT) {
+  delete(key: K) {
     delete this.data[key];
   }
 
   async clear() {
-    this.data = {} as Dict<KT, VT>;
+    this.data = {} as Dict<K, V>;
 
     await AsyncStorage.removeItem(this.key);
   }
 
-  values(): VT[] {
+  values(): V[] {
     return Object.values(this.data);
   }
 }
