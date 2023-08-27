5ee043f43e7aae76546bbadc898d8921d34bf0d8
Damego <damego.dev@gmail.com>
2023-08-27 19:17:30 +0500

    chore: bump version to beta 2

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'ЕТИС мобайл' + (IS_DEV ? ' (DEV)' : ''),
     slug: 'etis-mobile',
-    version: '1.1.0-beta.1',
+    version: '1.1.0-beta.2',
     owner: 'damego',
     orientation: 'portrait',
     icon: './assets/icon.png',
@@ -23,7 +23,7 @@ export default {
       supportsTablet: true,
     },
     android: {
-      versionCode: 10100000,
+      versionCode: 10100001,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF',
