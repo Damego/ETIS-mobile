4ff8f12b306c4196a44d6e75fb3b65c9955d2fb8
ijo42 <admin@Ijo42.ru>
2023-09-02 14:28:51 +0500

    chore: use immediate update

--- src/utils/inappUpdate.ts
+++ src/utils/inappUpdate.ts
@@ -1,26 +1,35 @@
 import Constants from 'expo-constants';
 import { Platform } from 'react-native';
-import SpInAppUpdates, { IAUUpdateKind, StartUpdateOptions } from 'sp-react-native-in-app-updates';
+import SpInAppUpdates, {
+  AndroidInstallStatus,
+  IAUUpdateKind,
+  StartUpdateOptions,
+} from 'sp-react-native-in-app-updates';
+import { AndroidInAppUpdateExtras } from 'sp-react-native-in-app-updates/lib/typescript/types';
 
-export const checkUpdate = () => {
-  const inAppUpdates = new SpInAppUpdates(
-    true // todo: disable
-  );
+const HIGH_PRIORITY_UPDATE = 5; // Arbitrary, depends on how you handle priority in the Play Console
+export const checkUpdate = (setReady) => {
+  const inAppUpdates = new SpInAppUpdates(false);
   console.log('[INAPP] Checking store version');
   inAppUpdates.checkNeedsUpdate({ curVersion: Constants.expoConfig.version }).then((result) => {
-    console.warn('[INAPP] result: ' + JSON.stringify(result));
+    console.log('[INAPP] result: ' + JSON.stringify(result));
     if (result.shouldUpdate) {
-      let updateOptions: StartUpdateOptions = {};
       if (Platform.OS === 'android') {
-        // android only, on iOS the user will be promped to go to your app store page
-        updateOptions = {
-          updateType: IAUUpdateKind.FLEXIBLE,
+        const updateOptions: StartUpdateOptions = {
+          updateType:
+            (result?.other as AndroidInAppUpdateExtras)?.updatePriority <= HIGH_PRIORITY_UPDATE // TODO: implement flexible
+              ? IAUUpdateKind.IMMEDIATE
+              : IAUUpdateKind.FLEXIBLE,
         };
+        inAppUpdates.addStatusUpdateListener((ev) => {
+          console.debug(`[INAPP] status: ${JSON.stringify(ev)}`);
+          if (ev.status === AndroidInstallStatus.DOWNLOADED) setReady(true);
+        });
+        inAppUpdates.startUpdate(updateOptions); // https://github.com/SudoPlz/sp-react-native-in-app-updates/blob/master/src/types.ts#L78
       }
-      inAppUpdates.addStatusUpdateListener((event) =>
-        console.warn(`[INAPP] ${JSON.stringify(event)}`)
-      );
-      inAppUpdates.startUpdate(updateOptions); // https://github.com/SudoPlz/sp-react-native-in-app-updates/blob/master/src/types.ts#L78
     }
   });
+  return inAppUpdates;
 };
+
+export const installUpdate = (spInAppUpdates: SpInAppUpdates) => spInAppUpdates.installUpdate();
