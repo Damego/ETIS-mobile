9678ed979952a64828bc4c14b00b8a9866b4280c
Damego <damego.dev@gmail.com>
2023-09-02 23:10:56 +0500

    refactor: messages

--- src/screens/messages/Messages.tsx
+++ src/screens/messages/Messages.tsx
@@ -1,73 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { ToastAndroid } from 'react-native';
+import React, { useRef } from 'react';
 
 import LoadingScreen from '../../components/LoadingScreen';
 import NoData from '../../components/NoData';
 import PageNavigator from '../../components/PageNavigator';
 import Screen from '../../components/Screen';
 import { useClient } from '../../data/client';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { IMessagesData } from '../../models/messages';
-import { GetResultType, RequestType } from '../../models/results';
-import { setAuthorizing } from '../../redux/reducers/authSlice';
+import { useAppDispatch } from '../../hooks';
+import useQuery from '../../hooks/useQuery';
+import { RequestType } from '../../models/results';
 import { setMessageCount } from '../../redux/reducers/studentSlice';
 import MessagePreview from './MessagePreview';
 
 const Messages = () => {
   const dispatch = useAppDispatch();
-  const { isAuthorizing } = useAppSelector((state) => state.auth);
-  const [data, setData] = useState<IMessagesData>();
-  const [isLoading, setLoading] = useState(false);
   const fetchedPages = useRef<number[]>([]);
   const client = useClient();
+  const { data, isLoading, refresh, update } = useQuery({
+    method: client.getMessagesData,
+    payload: {
+      requestType: RequestType.tryFetch,
+    },
+    after: (result) => {
+      if (!fetchedPages.current.includes(result.data.page)) {
+        fetchedPages.current.push(result.data.page);
+      }
 
-  const loadData = async ({ page, force }: { page?: number; force?: boolean }) => {
-    setLoading(true);
-    if (page === undefined) page = 1;
-
-    const result = await client.getMessagesData({
-      page,
-      requestType:
-        !force && fetchedPages.current.includes(page) ? RequestType.tryCache : RequestType.tryFetch,
+      if (!result.data) {
+        dispatch(setMessageCount(null));
+      }
+    },
+  });
+  const updateData = (page: number) => {
+    update({
+      data: page,
+      requestType: fetchedPages.current.includes(page)
+        ? RequestType.forceCache
+        : RequestType.forceFetch,
     });
-
-    if (result.type === GetResultType.loginPage) {
-      dispatch(setAuthorizing(true));
-      return;
-    }
-
-    if (!result.data) {
-      if (!data) setLoading(false);
-      ToastAndroid.show('Нет данных для отображения', ToastAndroid.LONG);
-      return;
-    }
-
-    setData(result.data);
-
-    if (!fetchedPages.current.includes(result.data.page)) {
-      fetchedPages.current.push(result.data.page);
-    }
-
-    if (!data) {
-      dispatch(setMessageCount(null));
-    }
-    setLoading(false);
   };
 
-  useEffect(() => {
-    if (!isAuthorizing) loadData({});
-  }, [isAuthorizing]);
-
-  if (isLoading) return <LoadingScreen onRefresh={() => loadData({})} />;
-  if (!data) return <NoData onRefresh={() => loadData({ force: true })} />;
+  if (isLoading) return <LoadingScreen onRefresh={refresh} />;
+  if (!data) return <NoData onRefresh={refresh} />;
 
   return (
-    <Screen onUpdate={() => loadData({ force: true })}>
+    <Screen onUpdate={refresh}>
       <PageNavigator
         firstPage={1}
         lastPage={data.lastPage}
         currentPage={data.page}
-        onPageChange={(page) => loadData({ page })}
+        onPageChange={updateData}
       />
 
       {data.messages.map((messageBlock) => (
