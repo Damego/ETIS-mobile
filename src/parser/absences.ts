7760ea966e48803f4ac96cb0335b713c3f5ec789
esperor <49198951+esperor@users.noreply.github.com>
2023-09-12 20:36:49 +0500

    refactor: optimize parseAbsences

--- src/parser/absences.ts
+++ src/parser/absences.ts
@@ -12,20 +12,20 @@ const parseDate = (date: string): IAbsenceDate => {
 export default function parseAbsences(html): IPeriodAbsences {
   const $ = load(html);
   let data: IDisciplineAbsences[] = [];
-  $('tr', html).each((index, element) => {
+
+  $('tr').each((index, element) => {
     if (index === 0) return;
 
-    const td = $(element).find('td');
+    const td = $('td', element);
     const fields = [];
+
+    // We will skip the number as it's not important info
+    // The dates
+    fields.push(td.eq(1).html().trim().split('<br>'));
     
-    // we will skip the number as it's not important info
-    for (let i = 1; i < 5; i += 1) {
-      if (i == 1) { // for the dates
-        fields[i - 1] = td.eq(i).html().trim().split('<br>');
-        continue;
-      }
-      fields[i - 1] = getTextField(td.eq(i));
-    }
+    for (let i = 2; i < 5; i += 1) 
+      fields.push(getTextField(td.eq(i)));
+
     const [dates, subject, type, teacher] = fields;
 
     data.push({
@@ -38,16 +38,17 @@ export default function parseAbsences(html): IPeriodAbsences {
 
   let period: number;
   let periods: string[] = [];
-  $('span.submenu-item', html).each((index, element) => {
+
+  $('span.submenu-item').each((index, element) => {
     let parsed = $(element);
     periods.push(parsed.text().trim());
 
     // define current period (current period's <a> doesn't have href attr)
-    if (parsed.find('a.dashed').attr('href') === undefined)
+    if (!parsed.find('a.dashed').attr('href'))
       period = index + 1;
   });
 
-  let overallMissed: string = $('div.span9', html).text().split(' ').at(-1);
+  let overallMissed: string = $('div.span9').text().split(' ').at(-1);
 
   return { period, periods, absences: data, overallMissed: parseInt(overallMissed) };
 }
