9761dd8d0a69e24a304cbb6e168b12b0a4735260
Damego <damego.dev@gmail.com>
2023-10-02 18:51:31 +0500

    refactor: apply updated ui to recovery

--- src/screens/auth/RecoveryForm.tsx
+++ src/screens/auth/RecoveryForm.tsx
@@ -4,11 +4,13 @@ import { Image, Text, TextInput, View } from 'react-native';
 import { Button, LoadingButton } from '../../components/Button';
 import ClickableText from '../../components/ClickableText';
 import { useGlobalStyles } from '../../hooks';
+import { useAppTheme } from '../../hooks/theme';
 import { fontSize } from '../../utils/texts';
 import { styles } from './AuthForm';
 
 const RecoveryForm = ({ onSubmit, isLoading, message, setShowModal, disabledRequestButton }) => {
   const globalStyles = useGlobalStyles();
+  const theme = useAppTheme();
 
   const [login, setLogin] = useState('');
 
@@ -24,11 +26,11 @@ const RecoveryForm = ({ onSubmit, isLoading, message, setShowModal, disabledRequ
           setLogin(newLogin);
         }}
         placeholder="Эл. почта"
-        placeholderTextColor={globalStyles.textColor.color}
+        placeholderTextColor={theme.colors.inputPlaceholder}
         autoComplete="email"
         inputMode="email"
         keyboardType="email-address"
-        selectionColor="#C62E3E"
+        selectionColor={theme.colors.primary}
         autoCapitalize="none"
         onSubmitEditing={() => onSubmit(login)}
       />
