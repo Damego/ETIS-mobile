1f7b69c442fe73c6062598653e56c8a315a198d8
Damego <damego.dev@gmail.com>
2023-10-12 21:10:15 +0500

    refactor(query): use 2 hooks

--- src/hooks/useSignsQuery.ts
+++ src/hooks/useSignsQuery.ts
@@ -1,27 +1,27 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { cache } from '../cache/smartCache';
 import { useClient } from '../data/client';
 import { composePointsAndMarks } from '../data/signs';
-import { GetResultType, IGetResult, RequestType } from '../models/results';
-import { ISessionMarks } from '../models/sessionMarks';
+import { GetResultType, RequestType } from '../models/results';
 import { setCurrentSession } from '../redux/reducers/studentSlice';
 import { useAppDispatch, useAppSelector } from './redux';
 import useQuery from './useQuery';
+import { ISessionPoints } from '../models/sessionPoints';
 
 const useSignsQuery = () => {
   const fetchedSessions = useRef<number[]>([]);
   const dispatch = useAppDispatch();
-  const sessionsMarks = useRef<ISessionMarks[]>([]);
   const { currentSession } = useAppSelector((state) => state.student);
   const client = useClient();
+  const [data, setData] = useState<ISessionPoints>(null);
 
-  const { data, isLoading, update, refresh } = useQuery({
+  const { data: pointsData, isLoading, update, refresh } = useQuery({
     method: client.getSessionSignsData,
     after: async (result) => {
       // Очевидно, что в самом начале мы получаем текущую сессию
       const { currentSession } = result.data;
-      if (!data) {
+      if (!pointsData) {
         dispatch(setCurrentSession(currentSession));
 
         if (result.type === GetResultType.fetched) {
@@ -32,21 +32,6 @@ const useSignsQuery = () => {
       if (!fetchedSessions.current.includes(currentSession)) {
         fetchedSessions.current.push(currentSession);
       }
-
-      // oh dear...
-      let marksResult: IGetResult<ISessionMarks[]>;
-      if (sessionsMarks.current.length === 0) {
-        marksResult = await client.getSessionMarksData({ requestType: RequestType.tryFetch });
-
-        if (marksResult.data) {
-          sessionsMarks.current = marksResult.data;
-        }
-      }
-
-      result.data = composePointsAndMarks(
-        result.data,
-        sessionsMarks.current.length !== 0 ? sessionsMarks.current : marksResult.data
-      );
     },
     onFail: async () => {
       const student = await cache.getStudent();
@@ -57,6 +42,15 @@ const useSignsQuery = () => {
       });
     },
   });
+
+  const marksQuery = useQuery({
+    method: client.getSessionMarksData,
+  })
+
+  useEffect(() => {
+    if (pointsData && marksQuery.data) setData(composePointsAndMarks(pointsData, marksQuery.data));
+  }, [pointsData, marksQuery.data]);
+
   const loadSession = (session: number) => {
     update({
       data: session,
