5f66ab6608d4321a6375e4c07e09af754cca960c
esperor <49198951+esperor@users.noreply.github.com>
2023-10-22 16:30:41 +0500

    fix: offline mode button (#133)

--- src/components/AuthLoadingModal.tsx
+++ src/components/AuthLoadingModal.tsx
@@ -178,6 +178,11 @@ const AuthLoadingModal = () => {
 
   return (
     <View style={styles.modalWrapper}>
+      <CustomReCaptcha
+        onReceiveToken={onReceiveToken}
+        size={isInvisibleRecaptcha ? 'invisible' : 'normal'}
+        onClose={onRecaptchaModalClose}
+      /> 
       <View
         style={[
           styles.modalContainer,
@@ -199,12 +204,7 @@ const AuthLoadingModal = () => {
               />
             </View>
           )}
-          <CustomReCaptcha
-            onReceiveToken={onReceiveToken}
-            size={isInvisibleRecaptcha ? 'invisible' : 'normal'}
-            onClose={onRecaptchaModalClose}
-          />
-        </View>
+        </View> 
       </View>
     </View>
   );
