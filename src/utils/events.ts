23b7170a3fda1507790812b821c20f515ad3f917
Damego <damego.dev@gmail.com>
2023-12-10 19:00:20 +0500

    feat: add new year event

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,19 +1,29 @@
 import { ThemeType } from '../styles/themes';
 
+export interface EventData {
+  suggestedTheme: boolean;
+  previousTheme: ThemeType;
+}
+
 export interface Events {
-  halloween2023?: {
-    suggestedTheme: boolean;
-    previousTheme: ThemeType;
-  };
+  halloween2023?: EventData;
+  newYear2024?: EventData;
+}
+
+const isInDateRange = (startDate: Date, endDate: Date) => {
+  const currentDateTime = Date.now();
+  const stateDateTime = startDate.getTime()
+  const endDateTime = endDate.getTime();
+
+  return currentDateTime > stateDateTime && currentDateTime < endDateTime;
 }
 
 export const isHalloween = () => {
-  const currentDate = Date.now();
   const year = new Date().getFullYear();
+  const startDate = new Date(year, 9, 28);
+  const endDate = new Date(year, 10, 4);
 
-  const startDate = new Date(year, 9, 28).getTime();
-  const endDate = new Date(year, 10, 4).getTime();
-  return currentDate > startDate && currentDate < endDate;
+  return isInDateRange(startDate, endDate);
 };
 
 export const halloweenEmptyDayResponses = [
@@ -26,3 +36,11 @@ export const halloweenEmptyDayResponses = [
   '🕸',
   'Отмечаем хэллоуин 🎃',
 ];
+
+export const isNewYear = () => {
+  const year = new Date().getFullYear();
+  const startDate = new Date(year, 11, 9);
+  const endDate = new Date(year + 1, 0, 15);
+
+  return isInDateRange(startDate, endDate);
+}
