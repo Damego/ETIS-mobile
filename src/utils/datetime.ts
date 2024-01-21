be5e9f75478b43c90adf7f8668a79df40fb8b98e
Damego <damego.dev@gmail.com>
2024-01-21 11:49:12 +0500

    fix: cathedra timetable for new year

---
+++ src/utils/datetime.ts
@@ -0,0 +1,12 @@
+import moment from 'moment/moment';
+
+export const getCurrentEducationYear = () => {
+  const date = moment();
+  const year = date.year();
+
+  // До сентября - старый учебный год
+  if (date.month() < 9) return year - 1;
+
+  // После сентября - новый
+  return year;
+}
