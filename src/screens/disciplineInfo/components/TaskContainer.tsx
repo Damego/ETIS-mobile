85a05719a14dedd729a1adff3c56d466b546d1bc
Damego <damego.dev@gmail.com>
2024-02-20 18:36:02 +0500

    feat: prevent screen close when modal opened

--- src/screens/disciplineInfo/components/TaskContainer.tsx
+++ src/screens/disciplineInfo/components/TaskContainer.tsx
@@ -1,9 +1,10 @@
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import dayjs from 'dayjs';
 import React, { useRef, useState } from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Text from '../../../components/Text';
+import useBackPress from '../../../hooks/useBackPress';
 import useTasks from '../../../hooks/useTasks';
 import { DisciplineStorage, DisciplineTask } from '../../../models/disciplinesTasks';
 import { fontSize } from '../../../utils/texts';
@@ -20,10 +21,21 @@ export const TaskContainer = ({
   disciplineDate: dayjs.Dayjs;
 }) => {
   const modalRef = useRef<BottomSheetModal>();
+  const modalOpened = useRef(false);
   const [selectedTask, setSelectedTask] = useState<DisciplineTask | null>(null);
   const { tasks, addTask, saveTasks, removeTask } = useTasks({
     filter: (task) => task.disciplineName === disciplineName,
   });
+
+  const openModal = () => {
+    modalRef.current.present();
+    modalOpened.current = true;
+  };
+
+  const closeModal = () => {
+    modalRef.current.dismiss();
+    modalOpened.current = false;
+  };
 
   const handleAddTask = (partial: PartialTask) => {
     if (selectedTask) {
@@ -57,13 +69,21 @@ export const TaskContainer = ({
     });
   };
 
+  useBackPress(() => {
+    if (modalOpened.current) {
+      closeModal();
+      return true;
+    }
+    return false;
+  });
+
   const currentDate = dayjs();
 
   return (
     <View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={[{ fontWeight: '500' }, fontSize.large]}>Задания</Text>
-        {disciplineDate > currentDate && <AddButton onPress={() => modalRef.current?.present()} />}
+      <View style={styles.taskContainer}>
+        <Text style={styles.taskText}>Задания</Text>
+        {disciplineDate > currentDate && <AddButton onPress={openModal} />}
       </View>
 
       <TaskList tasks={tasks} disciplineDate={disciplineDate} onRequestEdit={onRequestEdit} />
@@ -73,7 +93,21 @@ export const TaskContainer = ({
         onTaskAdd={handleAddTask}
         onTaskRemove={handleTaskRemove}
         task={selectedTask}
+        onDismiss={() => {
+          modalOpened.current = false;
+        }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  taskContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  taskText: {
+    fontWeight: '500',
+    ...fontSize.large,
+  },
+});
