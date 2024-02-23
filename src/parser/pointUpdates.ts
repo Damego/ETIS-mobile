ab6b7f9fcbea291b6b0ee9f04782e7ef64b836af
esperor <pere_strelka@mail.ru>
2024-02-23 16:17:30 +0500

    refactor: replace ' ' with \s in regex

--- src/parser/pointUpdates.ts
+++ src/parser/pointUpdates.ts
@@ -1,6 +1,6 @@
 import { executeRegex } from '../utils/sentry';
 
-const lastPointDateRegex = /([0-9]{2}\.[0-9]{2}\.[0-9]{4} )/m;
+const lastPointDateRegex = /([0-9]{2}\.[0-9]{2}\.[0-9]{4}\s)/m;
 
 export default function parsePointUpdates(data: string): string {
     if (data.length == 1) return null; 
