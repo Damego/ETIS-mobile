ce45ab84f1fc9eb9d105469977785bab02d0cfa7
Damego <damego.dev@gmail.com>
2024-03-05 14:07:07 +0500

    feat: show deadline task count in header button

--- src/navigation/headerButtons/DisciplineTasksButton.tsx
+++ src/navigation/headerButtons/DisciplineTasksButton.tsx
@@ -1,12 +1,22 @@
 import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import dayjs from 'dayjs';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
+import Text from '../../components/Text';
 import { useAppTheme } from '../../hooks/theme';
+import useTasks from '../../hooks/useTasks';
 import { RootStackNavigationProp } from '../types';
 
 const DisciplineTasksButton = () => {
+  const date = dayjs().startOf('day');
+  const { tasks } = useTasks({
+    filter: (task) => {
+      if (!task.datetime) return false;
+      return task.datetime.diff(date, 'day') === 1;
+    },
+  });
   const navigation = useNavigation<RootStackNavigationProp>();
   const theme = useAppTheme();
 
@@ -17,9 +27,30 @@ const DisciplineTasksButton = () => {
       }}
       style={{ justifyContent: 'center', marginRight: '7%' }}
     >
-      <AntDesign name="checkcircleo" size={28} color={theme.colors.primary} />
+      {tasks.length ? (
+        <View style={[styles.circle, { borderColor: theme.colors.primary }]}>
+          <Text style={[styles.text, { color: theme.colors.primary }]}>{tasks.length}</Text>
+        </View>
+      ) : (
+        <AntDesign name="checkcircleo" size={28} color={theme.colors.primary} />
+      )}
     </TouchableOpacity>
   );
 };
 
 export default DisciplineTasksButton;
+
+const styles = StyleSheet.create({
+  circle: {
+    width: 28,
+    height: 28,
+    borderRadius: 14,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: '500',
+    fontSize: 17,
+  },
+});
