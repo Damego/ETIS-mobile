dff5039cdfc0729906843b799b5ba268d71c6574
Damego <damego.dev@gmail.com>
2024-04-12 09:13:23 +0500

    refactor(teacher): replace tt buttons with context
    Removes timetable buttons for both teacher & cathedra and replaces them using context menu

--- src/screens/teachers/Teacher.tsx
+++ src/screens/teachers/Teacher.tsx
@@ -12,39 +12,12 @@ import {
 } from 'react-native';
 import ContextMenu from 'react-native-context-menu-view';
 
-import ClickableText from '../../components/ClickableText';
 import DisciplineType from '../../components/DisciplineType';
 import Text from '../../components/Text';
 import { ITeacher } from '../../models/teachers';
 import { ServicesNavigationProp } from '../../navigation/types';
 import { httpClient } from '../../utils';
 import { fontSize } from '../../utils/texts';
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-  },
-  teacherNameView: {},
-  textTitle: {
-    fontWeight: '500',
-    ...fontSize.medium,
-  },
-  subjectInfoView: {},
-  photoContainer: {},
-  photoStyle: {
-    width: 100,
-    height: 100,
-    borderRadius: 6,
-  },
-  teacherInfo: {
-    flex: 2,
-  },
-  teacherTimetableButton: { fontWeight: '500', textDecorationLine: 'underline' },
-  typesContainer: {
-    flexDirection: 'row',
-    gap: 4,
-  },
-});
 
 interface TeacherProps {
   discipline: string;
@@ -66,67 +39,106 @@ const Teacher = ({ discipline, data }: TeacherProps) => {
   const subject = data.subjects.find((sub) => sub.discipline === discipline);
 
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.teacherInfo}>
-          <View style={styles.teacherNameView}>
-            <ContextMenu
-              actions={[{ title: 'Скопировать' }, { title: 'Найти на сайте ПГНИУ' }]}
-              onPress={(event) => {
-                const pressIndex = event.nativeEvent.index;
+    <View style={styles.container}>
+      <View style={styles.teacherInfo}>
+        <View style={styles.teacherNameView}>
+          <ContextMenu
+            actions={[
+              { title: 'Скопировать' },
+              { title: 'Найти на сайте ПГНИУ' },
+              { title: 'Расписание пар' },
+            ]}
+            onPress={(event) => {
+              const pressIndex = event.nativeEvent.index;
 
-                if (pressIndex === 0) {
-                  Clipboard.setStringAsync(data.name);
-                  ToastAndroid.show('Скопировано в буфер обмена', ToastAndroid.LONG);
-                } else if (pressIndex === 1) {
-                  Linking.openURL(httpClient.getSearchPageURL(getSearchTeacherName(data.name)));
-                }
-              }}
-              dropdownMenuMode
-            >
-              {/* fake touchable effect */}
-              <TouchableOpacity>
-                <Text style={styles.textTitle} colorVariant={'block'}>
-                  {data.name}
-                </Text>
-              </TouchableOpacity>
-            </ContextMenu>
-            <View style={styles.typesContainer}>
-              {subject.types.map((type) => (
-                <DisciplineType key={type} type={type} size={'small'} />
-              ))}
-            </View>
-          </View>
-
-          <View style={styles.subjectInfoView}>
-            <Text style={fontSize.small} colorVariant={'block'}>
-              {data.cathedra}
-            </Text>
+              if (pressIndex === 0) {
+                Clipboard.setStringAsync(data.name);
+                ToastAndroid.show('Скопировано в буфер обмена', ToastAndroid.LONG);
+              } else if (pressIndex === 1) {
+                Linking.openURL(httpClient.getSearchPageURL(getSearchTeacherName(data.name)));
+              } else if (pressIndex === 2) {
+                navigation.navigate('CathedraTimetable', { teacherId: data.id });
+              }
+            }}
+            dropdownMenuMode
+          >
+            {/* fake touchable effect */}
+            <TouchableOpacity>
+              <Text style={styles.textTitle} colorVariant={'block'}>
+                {data.name}
+              </Text>
+            </TouchableOpacity>
+          </ContextMenu>
+          <View style={styles.typesContainer}>
+            {subject.types.map((type) => (
+              <DisciplineType key={type} type={type} size={'small'} />
+            ))}
           </View>
         </View>
 
-        {/* Фотография загружена... */}
-        <TouchableWithoutFeedback
-          style={styles.photoContainer}
-          onPress={() => ToastAndroid.show(data.photoTitle, ToastAndroid.SHORT)}
+        <ContextMenu
+          actions={[{ title: 'Скопировать' }, { title: 'Расписание пар' }]}
+          onPress={(event) => {
+            const pressIndex = event.nativeEvent.index;
+
+            if (pressIndex === 0) {
+              Clipboard.setStringAsync(data.name);
+              ToastAndroid.show('Скопировано в буфер обмена', ToastAndroid.LONG);
+            } else if (pressIndex === 1) {
+              navigation.navigate('CathedraTimetable', { cathedraId: data.cathedraId });
+            }
+          }}
+          dropdownMenuMode
         >
-          <Image
-            style={styles.photoStyle}
-            source={{
-              uri: `https://student.psu.ru/pls/stu_cus_et/${data.photo}`,
-            }}
-          />
-        </TouchableWithoutFeedback>
+          {/* fake touchable effect */}
+          <TouchableOpacity style={styles.subjectInfoView}>
+            <Text style={fontSize.small} colorVariant={'block'}>
+              {data.cathedra}
+            </Text>
+          </TouchableOpacity>
+        </ContextMenu>
       </View>
 
-      <ClickableText
-        text={'Расписание преподавателя'}
-        onPress={() => navigation.navigate('CathedraTimetable', { teacherId: data.id })}
-        textStyle={styles.teacherTimetableButton}
-        colorVariant={'block'}
-      />
-    </>
+      {/* Фотография загружена... */}
+      <TouchableWithoutFeedback
+        style={styles.photoContainer}
+        onPress={() => ToastAndroid.show(data.photoTitle, ToastAndroid.SHORT)}
+      >
+        <Image
+          style={styles.photoStyle}
+          source={{
+            uri: `https://student.psu.ru/pls/stu_cus_et/${data.photo}`,
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </View>
   );
 };
 
 export default Teacher;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  teacherNameView: {},
+  textTitle: {
+    fontWeight: '500',
+    ...fontSize.medium,
+  },
+  subjectInfoView: {},
+  photoContainer: {},
+  photoStyle: {
+    width: 100,
+    height: 100,
+    borderRadius: 6,
+  },
+  teacherInfo: {
+    flex: 2,
+  },
+  typesContainer: {
+    flexDirection: 'row',
+    gap: 4,
+  },
+});
--- src/screens/teachers/TeacherCard.tsx
+++ src/screens/teachers/TeacherCard.tsx
@@ -1,13 +1,9 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View } from 'react-native';
 
 import BorderLine from '../../components/BorderLine';
-import { Button } from '../../components/Button';
 import CardHeaderOut from '../../components/CardHeaderOut';
 import { ITeacher } from '../../models/teachers';
-import { ServicesNavigationProp } from '../../navigation/types';
-import { fontSize } from '../../utils/texts';
 import Teacher from './Teacher';
 
 interface TeacherCardProps {
@@ -15,28 +11,15 @@ interface TeacherCardProps {
   teachers: ITeacher[];
 }
 
-const TeacherCard = ({ discipline, teachers }: TeacherCardProps) => {
-  const navigation = useNavigation<ServicesNavigationProp>();
-
-  return (
-    <CardHeaderOut topText={discipline}>
-      {teachers.map((teacher, index) => (
-        <View key={teacher.name}>
-          <Teacher discipline={discipline} data={teacher} />
-          {index !== teachers.length - 1 ? <BorderLine /> : ''}
-        </View>
-      ))}
-      <BorderLine />
-      <Button
-        text={'Расписание кафедры'}
-        onPress={() =>
-          navigation.navigate('CathedraTimetable', { cathedraId: teachers[0].cathedraId })
-        }
-        variant={'card'}
-        fontStyle={fontSize.medium}
-      />
-    </CardHeaderOut>
-  );
-};
+const TeacherCard = ({ discipline, teachers }: TeacherCardProps) => (
+  <CardHeaderOut topText={discipline}>
+    {teachers.map((teacher, index) => (
+      <View key={teacher.name}>
+        <Teacher discipline={discipline} data={teacher} />
+        {index !== teachers.length - 1 ? <BorderLine /> : ''}
+      </View>
+    ))}
+  </CardHeaderOut>
+);
 
 export default TeacherCard;
