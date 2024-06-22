77db1072ff10e5d2944c1d53ff542e2530a1b84e
Damego <danyabatueff@gmail.com>
2024-06-22 18:47:57 +0500

    refactor: cleanup grades code

--- src/screens/etis/main/grades/Grades.tsx
+++ src/screens/etis/main/grades/Grades.tsx
@@ -1,61 +1,35 @@
 import { AntDesign } from '@expo/vector-icons';
 import { BottomSheetView } from '@gorhom/bottom-sheet';
-import React, { useMemo, useRef, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useMemo, useRef } from 'react';
+import { View } from 'react-native';
 import BorderLine from '~/components/BorderLine';
 import BottomSheetModal from '~/components/BottomSheetModal';
-import Card from '~/components/Card';
 import ClickableText from '~/components/ClickableText';
-import LoadingScreen from '~/components/LoadingScreen';
+import LoadingScreen, { LoadingContainer } from '~/components/LoadingScreen';
 import NoData from '~/components/NoData';
 import Screen from '~/components/Screen';
 import Text from '~/components/Text';
 import { useAppTheme } from '~/hooks/theme';
 import useSignsQuery from '~/hooks/useSignsQuery';
-import { ICheckPoint } from '~/models/sessionPoints';
-import Shortcuts, { Shortcut } from '~/screens/etis/main/components/Shortcuts';
-import { fontSize } from '~/utils/texts';
-
-import CardSign from './CardSign';
-
-const getOptions = ({
-  currentPeriod,
-  latestPeriod,
-  periodName,
-}: {
-  currentPeriod: number;
-  latestPeriod: number;
-  periodName: string;
-}) => {
-  const arr = Array.from(new Array(latestPeriod)).map((_, ind) => ind + 1);
-  return arr
-    .map((number) => ({
-      label: `${number} ${periodName}`,
-      value: number,
-      isCurrent: number === currentPeriod,
-    }))
-    .reverse();
-};
+import Shortcuts from '~/screens/etis/main/components/Shortcuts';
+import GradesContainer from '~/screens/etis/main/grades/GradesContainer';
+import PeriodsBottomSheet from '~/screens/etis/main/grades/PeriodsBottomSheet';
 
 const Grades = ({ onShortcutPress }) => {
   const { data, isLoading, refresh, loadSession } = useSignsQuery();
-  const theme = useAppTheme();
   const ref = useRef<BottomSheetModal>();
+  const theme = useAppTheme();
 
-  const options = useMemo(
-    () =>
-      data
-        ? getOptions({
-            currentPeriod: data.currentSession,
-            latestPeriod: data.latestSession,
-            periodName: data.sessionName,
-          })
-        : null,
-    [data]
-  );
+  let component: React.ReactNode;
 
-  if (isLoading && !data) return <LoadingScreen onRefresh={refresh} />;
-  if (!data) return <NoData onRefresh={refresh} />;
+  if (isLoading) {
+    component = <LoadingContainer />;
+  }
+  if (!data) {
+    component = <NoData onRefresh={refresh} />;
+  } else {
+    component = <GradesContainer data={data} />;
+  }
 
   const handlePeriodPress = (period: number) => {
     loadSession(period);
@@ -67,38 +41,21 @@ const Grades = ({ onShortcutPress }) => {
 
       <View style={{ flexDirection: 'row', gap: 14, alignItems: 'center', marginBottom: '2%' }}>
         <Text style={{ fontWeight: '700', fontSize: 22, flex: 2 }}>Оценки</Text>
-        <ClickableText
-          onPress={() => ref.current.present()}
-          textStyle={{ fontSize: 18 }}
-          iconRight={<AntDesign name="swap" size={18} color={theme.colors.text2} />}
-          viewStyle={{ gap: 4 }}
-        >
-          {data.currentSession} {data.sessionName}
-        </ClickableText>
+        {data && (
+          <ClickableText
+            onPress={() => ref.current.present()}
+            textStyle={{ fontSize: 18 }}
+            iconRight={<AntDesign name="swap" size={18} color={theme.colors.text2} />}
+            viewStyle={{ gap: 4 }}
+          >
+            {data.currentSession} {data.sessionName}
+          </ClickableText>
+        )}
       </View>
 
-      {data.subjects.map((subject, index) => (
-        <View key={subject.name}>
-          <CardSign subject={subject} />
-          {index !== data.subjects.length - 1 && <BorderLine />}
-        </View>
-      ))}
+      {component}
 
-      <BottomSheetModal ref={ref} onDismiss={() => ref.current.dismiss()}>
-        <BottomSheetView>
-          {options?.map((item) => (
-            <ClickableText
-              key={item.value}
-              onPress={() => handlePeriodPress(item.value)}
-              viewStyle={{ padding: '2%', width: '100%', justifyContent: 'center' }}
-              textStyle={{ fontSize: 18, fontWeight: '600' }}
-              colorVariant={item.isCurrent ? 'primary' : 'text2'}
-            >
-              {item.label}
-            </ClickableText>
-          ))}
-        </BottomSheetView>
-      </BottomSheetModal>
+      {data && <PeriodsBottomSheet ref={ref} data={data} onChange={handlePeriodPress} />}
     </Screen>
   );
 };
