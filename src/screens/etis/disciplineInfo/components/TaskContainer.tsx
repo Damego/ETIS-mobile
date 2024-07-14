67fb371b3f2b0776b78d78c721badc41be3e3d80
Damego <damego.dev@gmail.com>
2024-07-14 18:50:33 +0500

    chore(tasks): styling & modal stuff

--- src/screens/etis/disciplineInfo/components/TaskContainer.tsx
+++ src/screens/etis/disciplineInfo/components/TaskContainer.tsx
@@ -4,7 +4,6 @@ import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Text from '~/components/Text';
 import TaskContext from '~/context/taskContext';
-import useBackPress from '~/hooks/useBackPress';
 import useTasks from '~/hooks/useTasks';
 import { DisciplineTask } from '~/models/disciplinesTasks';
 import {
@@ -27,21 +26,10 @@ export const TaskContainer = ({
   disciplineDate: dayjs.Dayjs;
 }) => {
   const modalRef = useRef<BottomSheetModal>();
-  const modalOpened = useRef(false);
   const [selectedTask, setSelectedTask] = useState<DisciplineTask | null>(null);
   const { tasks, addTask, saveTasks, removeTask } = useTasks({
     filter: (task) => task.disciplineName === disciplineName,
   });
-
-  const openModal = () => {
-    modalRef.current.present();
-    modalOpened.current = true;
-  };
-
-  const closeModal = () => {
-    modalRef.current.dismiss();
-    modalOpened.current = false;
-  };
 
   const handleAddTask = ({ description, reminders, isLinkedToPair }: PartialTask) => {
     if (selectedTask) {
@@ -50,7 +38,7 @@ export const TaskContainer = ({
       selectedTask.reminders = reminders;
       rescheduleTaskNotifications(notificationIds, selectedTask);
       saveTasks().then(() => {
-        closeModal();
+        modalRef.current.dismiss();
         setSelectedTask(null);
       });
       return;
@@ -64,17 +52,17 @@ export const TaskContainer = ({
       false
     );
     scheduleTaskNotifications(task);
-    addTask(task).then(closeModal);
+    addTask(task).then(() => modalRef.current.dismiss());
   };
 
   const onRequestEdit = useCallback((task: DisciplineTask) => {
     setSelectedTask(task);
-    openModal();
+    modalRef.current.present();
   }, []);
 
   const handleTaskRemove = (task: DisciplineTask) => {
     cancelScheduledTaskNotifications({ task });
-    removeTask(task).then(closeModal);
+    removeTask(task).then(() => modalRef.current.dismiss());
   };
 
   const onTaskComplete = (task: DisciplineTask) => {
@@ -82,26 +70,13 @@ export const TaskContainer = ({
     saveTasks();
   };
 
-  // ВЕЗДЕ ПРОБЛЕМЫ
-  // 1. Библиотека bottom sheet modal не предоставляет информацию о состоянии модалки,
-  // поэтому используется ещё один ref
-  // 2. Ивент навигации внутри useBackPress не триггерится внутри модалки, поэтому,
-  // если выйти из модалки с добавлением напоминания, то она закроется, однако первая не появится :(
-  useBackPress(() => {
-    if (modalOpened.current) {
-      closeModal();
-      return true;
-    }
-    return false;
-  });
-
   const currentDate = dayjs();
 
   return (
     <View>
       <View style={styles.taskContainer}>
         <Text style={styles.taskText}>Задания</Text>
-        <AddButton onPress={openModal} />
+        <AddButton onPress={() => modalRef.current.present()} />
       </View>
 
       <TaskContext.Provider
@@ -118,9 +93,6 @@ export const TaskContainer = ({
         onTaskAdd={handleAddTask}
         onTaskRemove={handleTaskRemove}
         task={selectedTask}
-        onDismiss={() => {
-          modalOpened.current = false;
-        }}
         disableCheckbox={currentDate > disciplineDate || (selectedTask && !selectedTask.datetime)}
       />
     </View>
