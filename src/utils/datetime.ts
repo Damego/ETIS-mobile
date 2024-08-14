c4648d24e5e6eb47d1924afb4590df7fb30fabd3
Damego <damego.dev@gmail.com>
2024-08-14 21:52:23 +0500

    feat: methods to get first & last week dates

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -51,3 +51,14 @@ export const getEducationWeekByDate = (date: dayjs.Dayjs) => {
   const firstWeekDate = dayjs(new Date(getCurrentEducationYear(), 8, 2)).startOf('isoWeek');
   return date.startOf('isoWeek').diff(firstWeekDate, 'week') + 1;
 };
+
+
+export const getFirstEducationWeekDate = () => {
+  const year = getCurrentEducationYear();
+  return dayjs().startOf('day').startOf('week').year(year).month(8).day(2);
+}
+
+export const getLastEducationWeekDate = () => {
+  const year = getCurrentEducationYear() + 1;
+  return dayjs().year(year).month(8).date(1).startOf('day');
+}
