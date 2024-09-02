a8ed690bbb079cbc3ab45cfd525dd058ddf9ee74
Damego <damego.dev@gmail.com>
2024-09-02 19:11:25 +0500

    feat&ref: add timetable modes
    Опции "Выделять текущий день недели" и "Скрывать прошедшие дни недели" теперь доступны только, если режим расписания стоит как "По неделям".
    Перенесены компоненты в другую папку

--- src/screens/settings/uiSettings/components/ToggleHighlightCurrentDay.tsx
+++ src/screens/settings/uiSettings/components/ToggleHighlightCurrentDay.tsx
@@ -8,31 +8,29 @@ import { useAppTheme } from '~/hooks/theme';
 import { setUIConfig } from '~/redux/reducers/settingsSlice';
 import { fontSize } from '~/utils/texts';
 
-const ToggleShowPastWeekDays = () => {
+const ToggleHighlightCurrentDay = () => {
   const theme = useAppTheme();
-  const { showPastWeekDays } = useAppSelector((state) => state.settings.config.ui);
+  const { highlightCurrentDay } = useAppSelector((state) => state.settings.config.ui);
   const dispatch = useAppDispatch();
 
   const toggle = () => {
-    const config = { showPastWeekDays: !showPastWeekDays };
+    const config = { highlightCurrentDay: !highlightCurrentDay };
     dispatch(setUIConfig(config));
     cache.setUIConfig(config);
   };
 
   return (
     <Card style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Text style={[{ fontWeight: '500' }, fontSize.medium]} colorVariant={'block'}>
-        Скрывать прошедшие дни недели
-      </Text>
+      <Text style={[{ fontWeight: '500' }, fontSize.medium]}>Выделять текущий день недели</Text>
 
       <Switch
         trackColor={{ false: 'gray', true: theme.colors.primary }}
         thumbColor="white"
         onValueChange={toggle}
-        value={!showPastWeekDays}
+        value={highlightCurrentDay}
       />
     </Card>
   );
 };
 
-export default ToggleShowPastWeekDays;
+export default ToggleHighlightCurrentDay;
