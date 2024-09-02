f0f06169fd83122ef154e91a9f469785e37dd1bb
Damego <damego.dev@gmail.com>
2024-09-02 19:05:32 +0500

    fix: possible undefined teacher

--- src/utils/teachers.ts
+++ src/utils/teachers.ts
@@ -4,11 +4,11 @@ import { ITeacher as ITimeTableTeacher } from '~/models/timeTable';
 export const getTeacherName = (teachers: ITeacher[], teacher: ITimeTableTeacher) => {
   let teacherName: string;
 
-  if (teacher.id) {
-    const $teacher = teachers?.find(($teacher) => $teacher.id === teacher.id);
+  if (teacher?.id && teachers) {
+    const $teacher = teachers.find(($teacher) => $teacher.id === teacher.id);
     if ($teacher) teacherName = $teacher.name;
   }
-  if (!teacherName) teacherName = teacher.name;
+  if (!teacherName) teacherName = teacher?.name;
 
   return teacherName;
 };
