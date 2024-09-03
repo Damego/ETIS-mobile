4d4d6675021acbc2cbacf955ec48b3898efaa963
Damego <damego.dev@gmail.com>
2024-09-03 20:42:33 +0500

    refactor: one bottom sheet for all teachers

--- src/screens/etis/teachers/TeacherCard.tsx
+++ src/screens/etis/teachers/TeacherCard.tsx
@@ -9,13 +9,14 @@ import Teacher from './Teacher';
 interface TeacherCardProps {
   discipline: string;
   teachers: ITeacher[];
+  onPress: (teacher: ITeacher) => void;
 }
 
-const TeacherCard = ({ discipline, teachers }: TeacherCardProps) => (
+const TeacherCard = ({ discipline, teachers, onPress }: TeacherCardProps) => (
   <CardHeaderOut topText={discipline}>
     {teachers.map((teacher, index) => (
       <View key={teacher.name}>
-        <Teacher discipline={discipline} data={teacher} />
+        <Teacher discipline={discipline} data={teacher} onPress={onPress} />
         {index !== teachers.length - 1 ? <BorderLine /> : ''}
       </View>
     ))}
