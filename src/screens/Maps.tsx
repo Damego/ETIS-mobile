33680de6d7ac8b0a69d031a201477881facd47f1
Damego <damego.dev@gmail.com>
2025-03-01 23:56:47 +0500

    chore(maps): set styles with js; set theme

--- src/screens/Maps.tsx
+++ src/screens/Maps.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import AutoHeightWebView from 'react-native-autoheight-webview';
 import { useAppSelector } from '~/hooks';
+import { useAppTheme } from '~/hooks/theme';
 
-// Скрытие панели навигации
-const mapsStyle = `
-#root div:nth-child(1) {
-  flex: 0 0 100% !important;
+
+const getScript = (ical: string, theme: 'light' | 'dark') => `
+localStorage.setItem("ical_token", "${ical}");
+localStorage.setItem("theme", "${theme}");
+
+// Решения ниже не работают корректно если использовать css
+
+const getElementByXPath = (XPath) => {
+  const element = document.evaluate(XPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+  return element.singleNodeValue;
 }
 
-#root > div > div:nth-child(2) {
-  display: none !important;
-}
+// Удаление панели навигации
+const navElem = getElementByXPath("/html/body/div/div/div[2]");
+navElem.remove();
+
+// Задание полной высоты для оставшегося блока
+const searchElem = getElementByXPath("/html/body/div/div/div[1]");
+searchElem.style.flex = "0 0 100%";
 `;
-
-const getScript = (ical: string) => `localStorage.setItem("ical_token", "${ical}");`;
 
 function Maps() {
   const { iCalToken } = useAppSelector((state) => state.student);
+  const theme = useAppTheme();
 
   return (
     <AutoHeightWebView
@@ -24,8 +34,7 @@ function Maps() {
       source={{
         uri: `https://deploy-preview-86--psumaps-miniapp.netlify.app`,
       }}
-      customStyle={mapsStyle}
-      customScript={getScript(iCalToken)}
+      customScript={getScript(iCalToken, theme.dark ? 'dark' : 'light')}
       domStorageEnabled
     />
   );
