ec53f64a0cd5a042217c43057b1e9ce1347c59c0
Danil Batuev <damego.dev@gmail.com>
2025-12-15 19:10:32 +0500

    chore: bump version to 1.4.2-beta.1

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'ЕТИС мобайл' + (IS_DEV ? ' (DEV)' : ''),
     slug: 'etis-mobile',
-    version: '1.4.1',
+    version: '1.4.2-beta.1',
     owner: 'damego',
     orientation: 'portrait',
     icon: './assets/icon.png',
@@ -24,7 +24,7 @@ export default {
       supportsTablet: true,
     },
     android: {
-      versionCode: 10401004,
+      versionCode: 10402000,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF',
