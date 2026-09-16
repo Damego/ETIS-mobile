b81ab58a7a67efda3348b160266528e904c00871
ijo42 <admin@ijo42.ru>
2026-09-16 13:20:03 +0500

    bump: version bump

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'ЕТИС мобайл' + (IS_DEV ? ' (DEV)' : ''),
     slug: 'etis-mobile',
-    version: '1.5.0',
+    version: '1.5.1',
     owner: 'damego',
     orientation: 'portrait',
     icon: './assets/icon.png',
@@ -34,7 +34,7 @@ export default {
       supportsTablet: true,
     },
     android: {
-      versionCode: 10500000,
+      versionCode: 10501000,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF',
